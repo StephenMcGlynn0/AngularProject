964a78d02a7b217216830db3e44457e0c3e96791
StephenMcGlynn0 <l00176430@atu.ie>
2026-03-26 15:11:21 +0000

    Got express.js talking to the xampp database

--- 2026-restapi/index.js
+++ 2026-restapi/index.js
@@ -1,9 +1,35 @@
 import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
 
+let connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'gaanfl2026'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+app.get('/players', (req, res) =>{
+    connection.query('SELECT * FROM players', (err, rows, fields) => {
+  if (err) throw err
+
+console.log(rows)
+  res.send(rows)
+})
 })
 
 app.listen(3000, () => {
