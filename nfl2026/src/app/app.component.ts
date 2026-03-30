8559ffbf039716692e3c38fda052518c19b2bbc7
StephenMcGlynn0 <l00176430@atu.ie>
2026-03-31 00:00:21 +0100

    made the default route lead to RoutesPage, added router-outlet to bottom of navbar html

--- nfl2026/src/app/app.component.ts
+++ nfl2026/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RoutesPageComponent } from './routes-page/routes-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, RoutesPageComponent],
+  imports: [RouterOutlet, NavbarComponent, RoutesPageComponent, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- nfl2026/src/app/app.routes.ts
+++ nfl2026/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RoutesPageComponent } from './routes-page/routes-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: RoutesPageComponent, title: 'Home'},
+];
